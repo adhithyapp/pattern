0d59ca4cd27bde38a3d424517700bd7f2cad219e
adhithyapp <51515095+adhithyapp@users.noreply.github.com>
2019-08-20 14:32:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pattern
+{
+    public class Program
+    {
+            public static void Main(string[] args)
+            {
+                for (int row = 1; row <= 8; ++row)
+                {
+                    for (int col = 1; col <= row; ++col)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        
+    }
+}
